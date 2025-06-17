63aae351e6250b4ca3a83ed31accdd4f4f867c5f
JoshuaFrancoMasino <joshuamasino@outlook.com>
2025-06-17 10:44:38 +1000

    Updated ExploreModal.tsx

--- src/components/UI/ExploreModal.tsx
+++ src/components/UI/ExploreModal.tsx
@@ -699,3 +699,11 @@ const ExploreModal: React.FC<ExploreModalProps> = ({
         </div>
       </div>
     </div>
+  );
+};
+
+export default ExploreModal;
+
+export default ExploreModal
+
+export default ExploreModal
